50e7e0fca6e55760ab1518adead4a39b53e938db
jamesAforster <jamesalexforster@gmail.com>
2024-01-04 18:44:18 +0000

    Adds interpreter pattern

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Interpreter;
 
-Console.WriteLine("Hello, World!");
+Console.Title = "Interpreter";
+
+var expressions = new List<RomanExpression>
+{
+    new RomanHundredExpression(),
+    new RomanTenExpression(),
+    new RomanOneExpression(),
+};
+
+// var context = new RomanContext(5);
+//
+// foreach (var expression in expressions)
+// {
+//     expression.Interpret(context);
+// }
+//
+// Console.WriteLine($"Translating Arabic numerals to Roman numerals: {context.Output}");
+//
+// context = new RomanContext(81);
+//
+// foreach (var expression in expressions)
+// {
+//     expression.Interpret(context);
+// }
+
+// Console.WriteLine($"Translating Arabic numerals to Roman numerals: {context.Output}");
+
+var context = new RomanContext(733);
+
+foreach (var expression in expressions)
+{
+    expression.Interpret(context);
+}
+
+Console.WriteLine($"Translating Arabic numerals to Roman numerals: {context.Output}");
+
+Console.ReadKey();
